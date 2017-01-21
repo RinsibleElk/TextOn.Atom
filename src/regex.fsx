87f1e56bd7286c92b0a96509490f207ff494d445
RinsibleElk <oliver.samson@gmail.com>
2017-01-21 22:05:29 +0000

    sln file

--- src/regex.fsx
+++ src/regex.fsx
@@ -1,6 +1,6 @@
 open System.Text.RegularExpressions
 let s =
-  "var $Country =  \"Which country are you writing about?\",  [    *  ]";
+  "var $Country =\n  \"Which country are you writing about?\",\n  [\n    *\n  ]";
 let re =
-  new Regex("^(var)\\s+\\$[A-Za-z][A-Za-z0-9_]*")//"\\s*=\\S*(\".*\")\\s*,\\S*(\\[.*\\])")
+  new Regex("^(var)\\s+\\$[A-Za-z][A-Za-z0-9_]*\\s*=\\s*(\".*\")\\s*,\\s*(\\[\\s*\\*\\s*\\])")
 re.Match(s)
