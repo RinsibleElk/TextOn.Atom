f493c68b0276e60487997163b199fad57534a079
RinsibleElk <oliver.samson@gmail.com>
2017-01-22 16:14:46 +0000

    Not sure this is a great idea but hey

---
+++ TextOn.Atom.Test/TestCommentStripper.fs
@@ -0,0 +1,73 @@
+﻿module TextOn.Atom.Test.TestCommentStripper
+
+open FsUnit
+open FsCheck
+open NUnit.Framework
+open Swensen.Unquote
+
+open TextOn.Atom
+
+let test s e =
+    let r = CommentStripper.stripComments s
+    let firstFailingLine =
+        List.zip
+            (r |> List.truncate (e |> List.length))
+            (e |> List.truncate (r |> List.length))
+        |> List.tryFind (fun (r,e) -> r <> e)
+        |> Option.map (fun (r,e) -> sprintf "\n%A\n\n!=\n\n%A" e r)
+    if (r |> List.length) <> (e |> List.length) then
+        failwithf "The lists had different lengths - expected %d, result was %d, first failing line was: %A" (e |> List.length) (r |> List.length) firstFailingLine
+    else if firstFailingLine |> Option.isSome then
+        failwithf "The lists didn't match - first failing line was: %s" firstFailingLine.Value
+
+let exampleFileName = "example.texton"
+
+let example =
+    [
+        (false, "// Here's a function I can call from within the main.")
+        (true, "@func @guyStuff()")
+        (true, "{")
+        (true, "  @choice {")
+        (true, "    Blah.")
+        (true, "    Whatever.")
+        (true, "  }")
+        (true, "}")
+        (false, "")
+        (false, "    ")
+        (false, "  // Every full TextOn script must have a main function.   ")
+        (true, "@func @main()")
+        (true, "{")
+        (true, "  @seq {")
+        (false, "    // I can include comments at this level too. Note this poses a restriction that if you do want to start your line // we may have to invent an escape.")
+        (true, "    You are a bloke.")
+        (true, "    @guyStuff()")
+        (true, "  }")
+        (true, "}")
+        (false, "")
+    ]
+
+let alwaysFailFileResolver _ _ = None
+
+[<Test>]
+let ``CommentStripper with lines``() =
+    let expected =
+        example
+        |> List.scan
+            (fun (ln, _) (doInclude,line) ->
+                (ln + 1,
+                    Some (doInclude,
+                        {
+                            TopLevelFileLineNumber = ln
+                            CurrentFileLineNumber = ln
+                            CurrentFile = exampleFileName
+                            Contents = Line line})))
+            (1, None)
+        |> List.skip 1
+        |> List.map (snd >> Option.get)
+        |> List.filter fst
+        |> List.map snd
+    let input =
+        example
+        |> List.map snd
+        |> Preprocessor.preprocess alwaysFailFileResolver exampleFileName None
+    test input expected
---
+++ src/CommentStripper.fs
@@ -0,0 +1,17 @@
+﻿namespace TextOn.Atom
+
+open System.Text.RegularExpressions
+
+[<RequireQualifiedAccess>]
+module CommentStripper =
+    let private commentLineRegex = Regex(@"^\s*(//.*)?$")
+
+    /// Strip out all comment and blank lines from the preprocessed source.
+    let stripComments (lines:PreprocessedSourceLine list) : PreprocessedSourceLine list =
+        lines
+        |> List.filter
+            (fun line ->
+                let content = line.Contents
+                match content with
+                | Line line -> (not (commentLineRegex.IsMatch(line)))
+                | _ -> true)
