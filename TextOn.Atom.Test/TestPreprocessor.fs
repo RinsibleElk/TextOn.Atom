e55fd46e613f06d6e5964051ff6b7523d50e7a08
RinsibleElk <oliver.samson@gmail.com>
2017-01-22 15:47:13 +0000

    Implement very basic preprocessor

---
+++ TextOn.Atom.Test/TestPreprocessor.fs
@@ -0,0 +1,156 @@
+﻿module TextOn.Atom.Test.TestPreprocessor
+
+open FsUnit
+open FsCheck
+open NUnit.Framework
+open Swensen.Unquote
+
+open TextOn.Atom
+
+let alwaysFailFileResolver _ _ = None
+
+let exampleFileName = "example.texton"
+
+let test f s e =
+    let r = Preprocessor.preprocess f exampleFileName None s
+    let firstFailingLine =
+        List.zip
+            (r |> List.truncate (e |> List.length))
+            (e |> List.truncate (r |> List.length))
+        |> List.tryFind (fun (r,e) -> r <> e)
+        |> Option.map (fun (r,e) -> sprintf "\n%A\n\n!=\n\n%A" e r)
+    if (r |> List.length) <> (e |> List.length) then
+        failwithf "The lists had different lengths - expected %d, result was %d, first failing line was: %A" (e |> List.length) (r |> List.length) firstFailingLine
+    else if firstFailingLine |> Option.isSome then
+        failwithf "The lists didn't match - first failing line was: %s" firstFailingLine.Value
+
+let gender =
+    [
+        "@att %Gender ="
+        "  ["
+        "    \"Male\","
+        "    \"Female\""
+        "  ]"
+        "@att %Pronoun ="
+        "  ["
+        "    \"He\" [%Gender = \"Male\"],"
+        "    \"She\" [%Gender = \"Female\"]"
+        "  ]"
+    ]
+let example =
+    [
+        "// Here's a function I can call from within the main."
+        "@func @guyStuff()"
+        "{"
+        "  @choice {"
+        "    Blah."
+        "    Whatever."
+        "  }"
+        "}"
+        ""
+        "// Every full TextOn script must have a main function."
+        "@func @main()"
+        "{"
+        "  @seq {"
+        "    You are a bloke."
+        "    @guyStuff()"
+        "  }"
+        "}"
+    ]
+
+[<Test>]
+let ``Preprocessor with nothing to do``() =
+    test alwaysFailFileResolver [] []
+
+[<Test>]
+let ``Preprocessor with no includes``() =
+    let expected =
+        example
+        |> List.scan
+            (fun (ln,output) line ->
+                (ln + 1),
+                    Some {
+                        TopLevelFileLineNumber = ln
+                        CurrentFileLineNumber = ln
+                        CurrentFile = exampleFileName
+                        Contents = Line line
+                    })
+            (1,None)
+        |> List.skip 1
+        |> List.map (snd >> Option.get)
+    test alwaysFailFileResolver example expected
+
+[<Test>]
+let ``Preprocess with single successful include``() =
+    let source = "#include \"gender.texton\"" :: example
+    let fileResolver : PreprocessorFileResolver = (fun _ _ -> Some ("gender.texton", None, gender))
+    let expectedGender =
+        gender
+        |> List.scan
+            (fun (ln,output) line ->
+                (ln + 1),
+                    Some {
+                        TopLevelFileLineNumber = 1
+                        CurrentFileLineNumber = ln
+                        CurrentFile = "gender.texton"
+                        Contents = Line line
+                    })
+            (1,None)
+        |> List.skip 1
+        |> List.map (snd >> Option.get)
+    let expected =
+        source
+        |> List.scan
+            (fun (ln,output) line ->
+                (ln + 1),
+                    Some {
+                        TopLevelFileLineNumber = ln
+                        CurrentFileLineNumber = ln
+                        CurrentFile = exampleFileName
+                        Contents = Line line
+                    })
+            (1,None)
+        |> List.skip 2
+        |> List.map (snd >> Option.get)
+        |> fun l -> expectedGender @ l
+    test fileResolver source expected
+
+[<Test>]
+let ``Preprocess with double successful include``() =
+    let source = "#include \"gender.texton\"" :: ("#include \"gender.texton\"" :: example)
+    let fileResolver : PreprocessorFileResolver = (fun _ _ -> Some ("gender.texton", None, gender))
+    let expectedGender =
+        gender
+        |> List.scan
+            (fun (ln,output) line ->
+                (ln + 1),
+                    Some {
+                        TopLevelFileLineNumber = 1
+                        CurrentFileLineNumber = ln
+                        CurrentFile = "gender.texton"
+                        Contents = Line line
+                    })
+            (1,None)
+        |> List.skip 1
+        |> List.map (snd >> Option.get)
+    let expectedWarning = {
+        TopLevelFileLineNumber = 2
+        CurrentFileLineNumber = 2
+        CurrentFile = exampleFileName
+        Contents = Warning "Already included: gender.texton" }
+    let expected =
+        source
+        |> List.scan
+            (fun (ln,output) line ->
+                (ln + 1),
+                    Some {
+                        TopLevelFileLineNumber = ln
+                        CurrentFileLineNumber = ln
+                        CurrentFile = exampleFileName
+                        Contents = Line line
+                    })
+            (1,None)
+        |> List.skip 3
+        |> List.map (snd >> Option.get)
+        |> fun l -> expectedGender @ (expectedWarning ::l)
+    test fileResolver source expected
---
+++ src/Preprocessor.fs
@@ -0,0 +1,97 @@
+﻿namespace TextOn.Atom
+
+open System.Text.RegularExpressions
+open System.IO
+/// Service to resolve a file. Can be used in testing to not actually bother having real files lying around.
+type PreprocessorFileResolver = string -> string option -> (string * string option * string list) option
+
+// Loop through lines
+// If line contains a #include
+// - If include is invalid (no file specified), output a preprocessor error, and continue.
+// - If include is valid but file doesn't exist, output a preprocessor error, and continue.
+// - If include is valid and file exists, but file has already been included, output a preprocessor warning, and continue.
+// - Preprocess the inner and splice it in.
+
+type PreprocessorError = string
+type PreprocessorWarning = string
+type PreprocessorSuccess = string
+
+type PreprocessedLine =
+    | Error of PreprocessorError
+    | Warning of PreprocessorWarning
+    | Line of PreprocessorSuccess
+
+type PreprocessedOutput = {
+    TopLevelFileLineNumber : int
+    CurrentFileLineNumber : int
+    CurrentFile : string
+    Contents : PreprocessedLine }
+
+[<RequireQualifiedAccess>]
+module Preprocessor =
+    /// Regular expression to extract the file name from the #include directive.
+    let private includeRegex = Regex("^#include\\s+\"(.+)\"\s*$")
+
+    /// Stateful container for files that have already been included, earlier in the document (recursively).
+    type private IncludedFilesContainer() =
+        let mutable includedFiles = Set.empty
+        member __.AlreadyIncluded (s:string) = includedFiles |> Set.contains s
+        member __.Add s = includedFiles <- includedFiles |> Set.add s
+
+    /// Perform the preprocessing.
+    let rec private preprocessInner inTopLevelFile topLevelFileLineNumber currentFileLineNumber currentFile (fileResolver:PreprocessorFileResolver) (currentDirectory:string option) (includedFilesContainer:IncludedFilesContainer) (lines:string list) =
+        match lines with
+        | [] -> Seq.empty
+        | line::remaining ->
+            seq {
+                if (not (line.StartsWith("#"))) then
+                    yield {
+                        TopLevelFileLineNumber = topLevelFileLineNumber
+                        CurrentFileLineNumber = currentFileLineNumber
+                        CurrentFile = currentFile
+                        Contents = Line line }
+                else
+                    let includeMatch = includeRegex.Match(line)
+                    if (not (includeMatch.Success)) then
+                        yield {
+                            TopLevelFileLineNumber = topLevelFileLineNumber
+                            CurrentFileLineNumber = currentFileLineNumber
+                            CurrentFile = currentFile
+                            Contents = Error (line |> sprintf "Not a valid #include directive: %s") }
+                    else
+                        let includeFileUnresolved = includeMatch.Groups.[1].Value
+                        let resolvedFile = fileResolver includeFileUnresolved currentDirectory
+                        if (resolvedFile |> Option.isNone) then
+                            yield {
+                                TopLevelFileLineNumber = topLevelFileLineNumber
+                                CurrentFileLineNumber = currentFileLineNumber
+                                CurrentFile = currentFile
+                                Contents = Error (includeFileUnresolved |> sprintf "Unable to resolve file: %s") }
+                        else
+                            let (includeFileResolved, includeDirectory, includeLines) = resolvedFile |> Option.get
+                            if includedFilesContainer.AlreadyIncluded includeFileResolved then
+                                yield {
+                                    TopLevelFileLineNumber = topLevelFileLineNumber
+                                    CurrentFileLineNumber = currentFileLineNumber
+                                    CurrentFile = currentFile
+                                    Contents = Warning (includeFileResolved |> sprintf "Already included: %s") }
+                            else
+                                includedFilesContainer.Add includeFileResolved
+                                yield! preprocessInner false topLevelFileLineNumber 1 includeFileResolved fileResolver includeDirectory includedFilesContainer includeLines
+                yield! (preprocessInner inTopLevelFile (if inTopLevelFile then (topLevelFileLineNumber + 1) else topLevelFileLineNumber) (currentFileLineNumber + 1) currentFile fileResolver currentDirectory includedFilesContainer remaining) }
+
+    /// Get a "real" file resolver.
+    let realFileResolver : PreprocessorFileResolver =
+        (fun fileUnresolved directory ->
+            let file =
+                if directory |> Option.isSome then
+                    FileInfo(Path.Combine(directory.Value, fileUnresolved))
+                else
+                    FileInfo(fileUnresolved)
+            if file.Exists then Some (file.FullName, file.Directory.FullName |> Some, file.FullName |> File.ReadAllLines |> List.ofArray)
+            else None)
+
+    /// Perform the preprocess.
+    let preprocess fileResolver fileName currentDirectory lines =
+        preprocessInner true 1 1 fileName fileResolver currentDirectory (IncludedFilesContainer()) lines
+        |> List.ofSeq
