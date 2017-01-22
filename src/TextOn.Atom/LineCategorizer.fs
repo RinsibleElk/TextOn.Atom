dbd2d063497dacee86ae9ac1f40f3a83ebebb513
RinsibleElk <oliver.samson@gmail.com>
2017-01-22 23:26:54 +0000

    Still don't quite know where I'm going with this

--- src/TextOn.Atom/LineCategorizer.fs
+++ src/TextOn.Atom/LineCategorizer.fs
@@ -1,6 +1,15 @@
 ﻿namespace TextOn.Atom
 
+open System
+open System.Text.RegularExpressions
+
 [<RequireQualifiedAccess>]
 /// Maps preprocessed lines into categories.
 module LineCategorizer =
-    let a = 1
+    type private BracketCount = int
+    let private funcDefinitionRegex = Regex("^@func\s+")
+    let private varDefinitionRegex = Regex("^@var\s+")
+    let private attDefinitionRegex = Regex("^@att\s+")
+    let private openBraceRegex = Regex("\\s*\{\\s*$")
+    let private closeBraceRegex = Regex("\\s*\}\\s*$")
+
