8176e4bb133897a87919082600b55de5a1923a2c
RinsibleElk <oliver.samson@gmail.com>
2017-01-22 22:48:53 +0000

    Remove commas from var and att defns

--- src/TextOn.Atom/regex.fsx
+++ src/TextOn.Atom/regex.fsx
@@ -2,8 +2,3 @@
 #r "bin/Debug/TextOn.Atom.exe"
 open TextOn.Atom
 
-let source =
-    seq [
-        "#include \"D:\\NodeJs\TextOn.Atom\examples\cities.texton\""
-    ]
-let s = Preprocessor.preprocess Preprocessor.realFileResolver "" None source
