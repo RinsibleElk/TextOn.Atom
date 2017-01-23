bdf6a65bb36dbb19adeade4ec581560b5d624a8c
RinsibleElk <oliver.samson@gmail.com>
2017-01-23 19:51:28 +0000

    Change functions to no longer need the brackets

---
+++ src/TextOn.Atom/DefinitionLineTokenizer.fs
@@ -0,0 +1,10 @@
+﻿namespace TextOn.Atom
+
+open System
+open System.Text.RegularExpressions
+
+[<RequireQualifiedAccess>]
+/// Take a line that has been determined to be within a function definition and tokenize it.
+module DefinitionLineTokenizer =
+    // The tokens to find are '{', '|', '}', '[', ']', '=', '"', '@', '%'.
+    let tokenize () = ()
