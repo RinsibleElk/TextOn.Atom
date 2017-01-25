fbf43cbd0765f4e4511cf29fbe6776b95c595671
RinsibleElk <oliver.samson@gmail.com>
2017-01-25 23:21:31 +0000

    Quick condition resolver

--- src/TextOn.Atom/Agent.fsx
+++ src/TextOn.Atom/Agent.fsx
@@ -6,10 +6,13 @@ open System.Text.RegularExpressions
 
 type Message<'a,'b> =
     | Quit
-    | RegisterListener of ('b -> unit)
+    | Connect of ('b -> unit)
     | NewData of 'a
     | Fetch of 'b option AsyncReplyChannel
 type Agent<'a,'b> = Message<'a,'b> MailboxProcessor
+type EitherOr<'a,'b> =
+    | Either of 'a
+    | Or of 'b
 [<RequireQualifiedAccess>]
 module Agent =
     let map (f:'a -> 'b) (node:Agent<_,'a>) =
@@ -23,7 +26,7 @@ module Agent =
                             let! msg = inbox.Receive()
                             match msg with
                             | Quit -> return ()
-                            | RegisterListener f ->
+                            | Connect f ->
                                 listeners <- f::listeners
                                 return! loop()
                             | NewData a ->
@@ -37,7 +40,45 @@ module Agent =
                                 reply.Reply(b)
                                 return! loop() }
                     loop())
-        node.Post(RegisterListener(NewData >> agent.Post))
+        node.Post(Connect(NewData >> agent.Post))
+        agent
+    let map2 (f:'a -> 'b -> 'c) (node1:Agent<_,'a>) (node2:Agent<_,'b>) =
+        let nodeA = node1 |> map Either
+        let nodeB = node2 |> map Or
+        let agent =
+            Agent.Start
+                (fun inbox ->
+                    let mutable a = None
+                    let mutable b = None
+                    let mutable c = None
+                    let mutable listeners = []
+                    let rec loop() =
+                        async {
+                            let! msg = inbox.Receive()
+                            match msg with
+                            | Quit -> return ()
+                            | Connect f ->
+                                listeners <- f::listeners
+                                return! loop()
+                            | NewData data ->
+                                match data with
+                                | Either x ->
+                                    a <- Some x
+                                    if b.IsSome then
+                                        c <- Some (f x b.Value)
+                                        listeners |> List.iter (fun f -> f c.Value)
+                                | Or x ->
+                                    b <- Some x
+                                    if a.IsSome then
+                                        c <- Some (f a.Value x)
+                                        listeners |> List.iter (fun f -> f c.Value)
+                                return! loop()
+                            | Fetch(reply) ->
+                                reply.Reply(c)
+                                return! loop() }
+                    loop())
+        nodeA.Post(Connect(NewData >> agent.Post))
+        nodeB.Post(Connect(NewData >> agent.Post))
         agent
     let source() =
         Agent.Start
@@ -49,7 +90,7 @@ module Agent =
                         let! msg = inbox.Receive()
                         match msg with
                         | Quit -> return ()
-                        | RegisterListener f ->
+                        | Connect f ->
                             listeners <- f::listeners
                             return! loop()
                         | NewData a ->
@@ -66,18 +107,29 @@ module Agent =
     let fetch (agent:Agent<_,_>) =
         agent.PostAndReply(Fetch)
     let iter f (agent:Agent<_,_>) =
-        agent.Post(RegisterListener(f))
-    let newData data (agent:Agent<_,_>) =
+        agent.Post(Connect(f))
+    let post data (agent:Agent<_,_>) =
         agent.Post(NewData(data))
+
+// Set up pipeline.
 let source          = Agent.source()
 let preprocessor    = source |> Agent.map (fun (a,b,c:string seq) -> Preprocessor.preprocess Preprocessor.realFileResolver a b c)
 let stripper        = preprocessor |> Agent.map CommentStripper.stripComments
 let categorizer     = stripper |> Agent.map LineCategorizer.categorize
 let tokenizer       = categorizer |> Agent.map (fun s -> s |> Seq.map (fun x -> async { return Tokenizer.tokenize x }) |> Async.Parallel |> Async.RunSynchronously)
-let printer         = tokenizer |> Agent.iter (printfn "%A")
-let post a b c      = source.Post(NewData(a,b,c))
-let f = FileInfo(@"D:\NodeJs\TextOn.Atom\examples\example.texton")
-let directory = f.Directory.FullName |> Some
-let file = f.Name
-let lines = f.FullName |> File.ReadAllLines |> Seq.ofArray
-source |> Agent.newData (file,directory,lines)
+
+// Example data.
+let f               = FileInfo(@"D:\NodeJs\TextOn.Atom\examples\example.texton")
+let directory       = f.Directory.FullName |> Some
+let file            = f.Name
+let lines           = f.FullName |> File.ReadAllLines |> Seq.ofArray
+source |> Agent.post (file,directory,lines)
+
+// Retrieve result.
+let result          =
+    tokenizer
+    |> Agent.fetch
+    |> Option.get
+    |> Seq.collect Option.get
+    |> Seq.concat
+
---
+++ src/TextOn.Atom/Condition.fs
@@ -0,0 +1,24 @@
+﻿namespace TextOn.Atom
+
+/// Unique identifier for an attribute in some cache we prepare.
+type AttributeIdentity = int
+
+/// Represents a tree of decisions.
+type Condition =
+    | True
+    | Both of Condition * Condition
+    | Either of Condition * Condition
+    | AreEqual of AttributeIdentity * string
+    | AreNotEqual of AttributeIdentity * string
+
+/// Resolve a condition from a cache of values.
+[<RequireQualifiedAccess>]
+module ConditionResolver =
+    /// Resolve a condition's value.
+    let rec resolve (cache:Map<int, string>) condition : bool =
+        match condition with
+        | True -> true
+        | Both(c1, c2) -> (c1 |> resolve cache) && (c2 |> resolve cache)
+        | Either(c1, c2) -> (c1 |> resolve cache) || (c2 |> resolve cache)
+        | AreEqual(identity, value) -> (cache |> Map.find identity) = value
+        | AreNotEqual(identity, value) -> (cache |> Map.find identity) <> value
