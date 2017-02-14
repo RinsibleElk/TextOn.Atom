fd2e857a766c2acb943bb82132547eca2dafd17e
RinsibleElk <oliver.samson@gmail.com>
2017-02-14 20:02:27 +0000

    Back out incorrect comment change from earlier

--- src/TextOn.Atom/CommentStripper.fs
+++ src/TextOn.Atom/CommentStripper.fs
@@ -7,19 +7,16 @@ open System.Text.RegularExpressions
 /// Maps preprocessed source into preprocessed source having removed comments.
 /// OPS: It's not obvious this is sensible prior to tokenizing. Note this basically assumes that comments are whole lines.
 module CommentStripper =
-    let private singleLineCommentRegex = Regex("(^|^(.+?[^\\\\]))(\\s*//.*)?$")
-    let private matchEvaluator = MatchEvaluator(fun m -> m.Groups.[1].Value)
-    let replace s = singleLineCommentRegex.Replace(s, matchEvaluator)
+    let private wholeLineCommentRegex = Regex("^\\s*//")
 
     /// Strip out all comment and blank lines from the preprocessed source.
     let stripComments (lines:PreprocessedSourceLine list) : PreprocessedSourceLine list =
         lines
-        |> List.choose
+        |> List.filter
             (fun line ->
                 let content = line.Contents
                 match content with
                 | PreprocessorLine l ->
-                    let l = l |> replace
-                    if String.IsNullOrWhiteSpace(l) then None
-                    else Some { line with Contents = PreprocessorLine l }
-                | _ -> Some line)
+                    if String.IsNullOrWhiteSpace l then false
+                    else (not (wholeLineCommentRegex.IsMatch l))
+                | _ -> true)
