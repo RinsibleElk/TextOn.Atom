aab57304e761862c0ce725cb88f93b22b1c685da
RinsibleElk <oliver.samson@gmail.com>
2017-07-01 15:00:58 +0100

    Finish the new tokenizer

--- src/TextOn.Core.Test/TestTokenizer.fs
+++ src/TextOn.Core.Test/TestTokenizer.fs
@@ -1,4 +1,4 @@
-﻿module TextOn.Core.Test.Tokenizer
+﻿module TextOn.Core.Test.TestTokenizer
 
 open System
 open FsUnit
@@ -45,22 +45,61 @@ let private runTest input =
     let result = Tokenizer.tokenize exampleFileName lines
     test <@ result = expected @>
 
+let private funcLines =
+    [
+        (CategorizedFuncDefinition,
+            [
+                ("@func @main", [(Func, 1, 5); (FunctionName "main", 7, 11)])
+                ("{", [(OpenCurly, 1, 1)])
+                ("  @seq {", [(Sequential, 3, 6);(OpenCurly, 8, 8)])
+                ("    You are a bloke. [%Gender = \"Male\"]", [(RawText "You are a bloke.", 5, 20); (OpenBrace, 22, 22); (AttributeName "Gender", 23, 29); (Equals, 31, 31); (QuotedString "Male", 33, 38); (CloseBrace, 39, 39)])
+                ("    You live in {$City|a city in $Country}.", [(RawText "You live in ", 5, 16); (OpenCurly, 17, 17); (VariableName "City", 18, 22); (ChoiceSeparator, 23, 23); (RawText "a city in ", 24, 33); (VariableName "Country", 34, 41); (CloseCurly, 42, 42); (RawText ".", 43, 43)])
+                ("    We are in $City which is in $Country.", [(RawText "We are in ", 5, 14); (VariableName "City", 15, 19); (RawText " which is in ", 20, 32); (VariableName "Country", 33, 40); (RawText ".", 41, 41)])
+                ("    @break", [(Break, 5, 10)])
+                ("    @guyStuff [%Gender = \"Male\"]", [(FunctionName "guyStuff", 5, 13); (OpenBrace, 15, 15); (AttributeName "Gender", 16, 22); (Equals, 24, 24); (QuotedString "Male", 26, 31); (CloseBrace, 32, 32)])
+                ("  }", [(CloseCurly, 3, 3)])
+                ("}", [(CloseCurly, 1, 1)])
+            ])
+    ]
+
+let private varLines =
+    [
+        (CategorizedVarDefinition,
+            [
+                ("@var @free $City =", [(Var, 1, 4);(Free, 6, 10);(VariableName "City", 12, 16);(Equals, 18, 18)])
+                ("  \"Which city are you writing about?\"", [(QuotedString "Which city are you writing about?", 3, 37)])
+                ("  {", [(OpenCurly, 3, 3)])
+                ("    \"London\" [$Country = \"U.K.\"] // Comment after condition", [(QuotedString "London", 5, 12);(OpenBrace, 14, 14);(VariableName("Country"), 15, 22);(Equals, 24, 24);(QuotedString "U.K.", 26, 31);(CloseBrace, 32, 32)])
+                ("    \"Berlin\" [$Country = \"Germany\" || %Gender = \"Male\"]", [(QuotedString "Berlin", 5, 12);(OpenBrace, 14, 14);(VariableName("Country"), 15, 22);(Equals, 24, 24);(QuotedString "Germany", 26, 34);(Or, 36, 37);(AttributeName "Gender", 39, 45);(Equals, 47, 47);(QuotedString "Male", 49, 54);(CloseBrace, 55, 55)])
+                ("    \"Paris\" [$Country <> \"Germany\" && $Country <> \"U.K.\"]", [(QuotedString "Paris", 5, 11);(OpenBrace, 13, 13);(VariableName("Country"), 14, 21);(NotEquals, 23, 24);(QuotedString "Germany", 26, 34);(And, 36, 37);(VariableName("Country"), 39, 46);(NotEquals, 48, 49);(QuotedString "U.K.", 51, 56);(CloseBrace, 57, 57)])
+                ("  }", [(CloseCurly, 3, 3)])
+            ])
+    ]
+
+let private attLines =
+    [
+        (CategorizedAttDefinition,
+            [
+                ("@att %Gender = \"What is the gender of your target audience?\"", [(Att, 1, 4);(AttributeName "Gender", 6, 12);(Equals, 14, 14);(QuotedString "What is the gender of your target audience?", 16, 60)])
+                ("  {", [(OpenCurly, 3, 3)])
+                ("    \"Male\" // Comment after no condition", [(QuotedString "Male", 5, 10)])
+                ("    \"Female\"", [(QuotedString "Female", 5, 12)])
+                ("  }", [(CloseCurly, 3, 3)])
+            ])
+    ]
+
 [<Test>]
 let ``Valid full function``() =
-    let lines =
-        [
-            (CategorizedFuncDefinition,
-                [
-                    ("@func @main", [(Func, 1, 5); (FunctionName "main", 7, 11)])
-                    ("{", [(OpenCurly, 1, 1)])
-                    ("  @seq {", [(Sequential, 3, 6);(OpenCurly, 8, 8)])
-                    ("    You are a bloke. [%Gender = \"Male\"]", [(RawText "You are a bloke.", 5, 20); (OpenBrace, 22, 22); (AttributeName "Gender", 23, 29); (Equals, 31, 31); (QuotedString "Male", 33, 38); (CloseBrace, 39, 39)])
-                    ("    You live in {$City|a city in $Country}.", [(RawText "You live in ", 5, 16); (OpenCurly, 17, 17); (VariableName "City", 18, 22); (ChoiceSeparator, 23, 23); (RawText "a city in ", 24, 33); (VariableName "Country", 34, 41); (CloseCurly, 42, 42); (RawText ".", 43, 43)])
-                    ("    We are in $City which is in $Country.", [(RawText "We are in ", 5, 14); (VariableName "City", 15, 19); (RawText " which is in ", 20, 32); (VariableName "Country", 33, 40); (RawText ".", 41, 41)])
-                    ("    @break", [(Break, 5, 10)])
-                    ("    @guyStuff [%Gender = \"Male\"]", [(FunctionName "guyStuff", 5, 13); (OpenBrace, 15, 15); (AttributeName "Gender", 16, 22); (Equals, 24, 24); (QuotedString "Male", 26, 31); (CloseBrace, 32, 32)])
-                    ("  }", [(CloseCurly, 3, 3)])
-                    ("}", [(CloseCurly, 1, 1)])
-                ])
-        ]
-    runTest lines
+    runTest funcLines
+
+[<Test>]
+let ``Valid free variable``() =
+    runTest varLines
+
+[<Test>]
+let ``Valid attribute``() =
+    runTest attLines
+
+[<Test>]
+let ``Valid attribute, variable, then function``() =
+    runTest (attLines@varLines@funcLines)
--- src/TextOn.Core/Tokenizer.fs
+++ src/TextOn.Core/Tokenizer.fs
@@ -4,7 +4,9 @@ module TextOn.Core.Tokenizer
 type private TokenizerState =
     | Done
     | Outside of int
+    | Error of int * int * int * AttributedTokenizedLine list
     | Function of int * int * int * int * AttributedTokenizedLine list
+    | VariableOrAttribute of Category * int * int * int * int * AttributedTokenizedLine list
 
 let private makeAttributedLine line tokens =
     {
@@ -23,11 +25,18 @@ let private makeTokenSet category fileName startLine endLine tokens =
 
 let private makeError error = makeTokenSet (CategorizationError error)
 let private makeFunction = makeTokenSet CategorizedFuncDefinition
+let private makeImport fileName line tokens = makeTokenSet CategorizedImport fileName line line (tokens |> makeAttributedLine line |> List.singleton)
 
 let private transitionToFunction line (tokens:_ list) =
     (Function (line, line, line + 1, (if tokens.[tokens.Length - 1].Token = OpenCurly then 1 else 0), tokens |> makeAttributedLine line |> List.singleton))
+let private transitionToVariableOrAttribute category line (tokens:_ list) =
+    (VariableOrAttribute (category, line, line, line + 1, (if tokens.[tokens.Length - 1].Token = OpenCurly then 1 else 0), tokens |> makeAttributedLine line |> List.singleton))
 let private transitionToOutside line =
     Outside line
+
+let private errorBlockText = "Unrecognised starting token"
+let private incompleteFunctionText = "Incomplete function definition"
+let private incompleteVarText = "Incomplete variable/attribute definition"
 
 let rec private tokenizeInner fileName state lines output =
     let newState, newLines, newOutput =
@@ -40,32 +49,79 @@ let rec private tokenizeInner fileName state lines output =
                 Outside (line + 1), t, output
             else
                 match tokens.[0].Token with
-                | Func ->
-                    transitionToFunction line tokens, t, output
-                | _ ->
-                    // TODO
-                    Done, [], output
+                | Func -> transitionToFunction line tokens, t, output
+                | Var -> transitionToVariableOrAttribute CategorizedVarDefinition line tokens, t, output
+                | Att -> transitionToVariableOrAttribute CategorizedAttDefinition line tokens, t, output
+                | Import
+                | Include -> Outside (line + 1), t, ((makeImport fileName line tokens)::output)
+                | _ -> Error(line, line, line + 1, (tokens |> makeAttributedLine line |> List.singleton)), t, output
+        | Error(startLine, endLine, currentLine, tokenizedLines), [] ->
+            Done, [], ((makeError errorBlockText fileName startLine endLine tokenizedLines)::output)
+        | Error(startLine, endLine, currentLine, tokenizedLines), h::t ->
+            let tokens = OutsideLineTokenizer.tokenizeLine h
+            if tokens.Length = 0 then
+                Error(startLine, endLine, currentLine + 1, tokenizedLines), t, output
+            else
+                match tokens.[0].Token with
+                | Func -> transitionToFunction currentLine tokens, t, ((makeError errorBlockText fileName startLine endLine tokenizedLines)::output)
+                | Var -> transitionToVariableOrAttribute CategorizedVarDefinition currentLine tokens, t, ((makeError errorBlockText fileName startLine endLine tokenizedLines)::output)
+                | Att -> transitionToVariableOrAttribute CategorizedAttDefinition currentLine tokens, t, ((makeError errorBlockText fileName startLine endLine tokenizedLines)::output)
+                | Import
+                | Include -> Outside (currentLine + 1), t, ((makeImport fileName currentLine tokens)::((makeError errorBlockText fileName startLine endLine tokenizedLines)::output))
+                | _ -> Error(startLine, endLine, currentLine + 1, (tokens |> makeAttributedLine currentLine)::tokenizedLines), t, output
         | Function (startLine, endLine, currentLine, numBrackets, tokenizedLines), [] ->
             // Output the incomplete function as an error and transition to Done state
-            Done, [], (makeError "Incomplete function" fileName startLine endLine tokenizedLines)::output
+            Done, [], (makeError incompleteFunctionText fileName startLine endLine tokenizedLines)::output
+        | VariableOrAttribute (category, startLine, endLine, currentLine, numBrackets, tokenizedLines), [] ->
+            // Output the incomplete variable or attribute as an error and transition to Done state
+            Done, [], (makeError incompleteVarText fileName startLine endLine tokenizedLines)::output
         | Function (startLine, endLine, currentLine, numBrackets, tokenizedLines), h::t ->
             // Tokenize the line as a function line.
             let tokens = FunctionLineTokenizer.tokenizeLine h
             if tokens.Length = 0 then
                 Function(startLine, endLine, currentLine + 1, numBrackets, tokenizedLines), t, output
             else
-                // TODO First we detect if the user has moved onto something new but screwed up.
-
+                // First we detect if the user has moved onto something new but screwed up.
+                match tokens.[0].Token with
+                | Func -> transitionToFunction currentLine tokens, t, ((makeError incompleteFunctionText fileName startLine endLine tokenizedLines)::output)
+                | Var -> transitionToVariableOrAttribute CategorizedVarDefinition currentLine tokens, t, ((makeError incompleteFunctionText fileName startLine endLine tokenizedLines)::output)
+                | Att -> transitionToVariableOrAttribute CategorizedAttDefinition currentLine tokens, t, ((makeError incompleteFunctionText fileName startLine endLine tokenizedLines)::output)
+                | Import
+                | Include -> Outside (currentLine + 1), t, ((makeImport fileName currentLine tokens)::((makeError incompleteFunctionText fileName startLine endLine tokenizedLines)::output))
                 // Has the bracket count changed?
-                if tokens.[0].Token = CloseCurly then
+                | CloseCurly ->
                     if numBrackets = 1 then
                         transitionToOutside (currentLine + 1), t, (makeFunction fileName startLine currentLine ((makeAttributedLine currentLine tokens)::tokenizedLines))::output
                     else
                         Function(startLine, currentLine, currentLine + 1, numBrackets - 1, (makeAttributedLine currentLine tokens)::tokenizedLines), t, output
-                else if tokens.[tokens.Length - 1].Token = OpenCurly then
-                    Function(startLine, currentLine, currentLine + 1, numBrackets + 1, (makeAttributedLine currentLine tokens)::tokenizedLines), t, output
-                else
-                    Function(startLine, currentLine, currentLine + 1, numBrackets, (makeAttributedLine currentLine tokens)::tokenizedLines), t, output
+                | _ ->
+                    if tokens.[tokens.Length - 1].Token = OpenCurly then
+                        Function(startLine, currentLine, currentLine + 1, numBrackets + 1, (makeAttributedLine currentLine tokens)::tokenizedLines), t, output
+                    else
+                        Function(startLine, currentLine, currentLine + 1, numBrackets, (makeAttributedLine currentLine tokens)::tokenizedLines), t, output
+        | VariableOrAttribute (category, startLine, endLine, currentLine, numBrackets, tokenizedLines), h::t ->
+            // Tokenize the line as a variable line.
+            let tokens = VariableOrAttributeLineTokenizer.tokenizeLine h
+            if tokens.Length = 0 then
+                VariableOrAttribute(category, startLine, endLine, currentLine + 1, numBrackets, tokenizedLines), t, output
+            else
+                match tokens.[0].Token with
+                | Func -> transitionToFunction currentLine tokens, t, ((makeError incompleteVarText fileName startLine endLine tokenizedLines)::output)
+                | Var -> transitionToVariableOrAttribute CategorizedVarDefinition currentLine tokens, t, ((makeError incompleteVarText fileName startLine endLine tokenizedLines)::output)
+                | Att -> transitionToVariableOrAttribute CategorizedAttDefinition currentLine tokens, t, ((makeError incompleteVarText fileName startLine endLine tokenizedLines)::output)
+                | Import
+                | Include -> Outside (currentLine + 1), t, ((makeImport fileName currentLine tokens)::((makeError incompleteVarText fileName startLine endLine tokenizedLines)::output))
+                // Has the bracket count changed?
+                | CloseCurly ->
+                    if numBrackets = 1 then
+                        transitionToOutside (currentLine + 1), t, (makeTokenSet category fileName startLine currentLine ((makeAttributedLine currentLine tokens)::tokenizedLines))::output
+                    else
+                        VariableOrAttribute(category, startLine, currentLine, currentLine + 1, numBrackets - 1, (makeAttributedLine currentLine tokens)::tokenizedLines), t, output
+                | _ ->
+                    if tokens.[tokens.Length - 1].Token = OpenCurly then
+                        VariableOrAttribute(category, startLine, currentLine, currentLine + 1, numBrackets + 1, (makeAttributedLine currentLine tokens)::tokenizedLines), t, output
+                    else
+                        VariableOrAttribute(category, startLine, currentLine, currentLine + 1, numBrackets, (makeAttributedLine currentLine tokens)::tokenizedLines), t, output
     if newState = Done then
         newOutput |> List.rev
     else
