946678775e97aade86d1124021ab932080b72c1b
Oliver Samson <oliver.samson@gmail.com>
2017-07-02 23:55:37 +0100

    Vague attempt at finding all errors that are possible in a template

--- src/TextOn.Core/Linking/Linker.fs
+++ src/TextOn.Core/Linking/Linker.fs
@@ -63,15 +63,55 @@ module Linker =
         |> Set.toList
         |> List.choose (fun f -> knownFiles |> Map.tryFind f)
 
-    let rec private missingFunctionReferences file functionName node =
+    let rec private missingReferencesInCondition file missingAttributes missingVariables condition =
+        match condition with
+        | ParsedAnd(c1, c2) -> List.append (missingReferencesInCondition file missingAttributes missingVariables c1) (missingReferencesInCondition file missingAttributes missingVariables c2)
+        | ParsedOr(c1, c2) -> List.append (missingReferencesInCondition file missingAttributes missingVariables c1) (missingReferencesInCondition file missingAttributes missingVariables c2)
+        | ParsedAreNotEqual(l,s,e,v,_)
+        | ParsedAreEqual(l,s,e,v,_) ->
+            match v with
+            | ParsedVariableName v ->
+                if missingVariables |> Set.contains v then [makeParseError file l s e (sprintf "Unknown variable %s" v)] else []
+            | ParsedAttributeName v ->
+                if missingAttributes |> Set.contains v then [makeParseError file l s e (sprintf "Unknown attribute %s" v)] else []
+        | _ -> []
+
+    let rec private missingReferencesInSentence file line missingVariables node =
         match node with
-        | ParsedFunctionInvocation (l,s,e,f) -> if f = functionName then [makeParseError file l s e (sprintf "Unknown function %s" f)] else []
+        | ParsedSentenceNode.ParsedSimpleChoice l
+        | ParsedSentenceNode.ParsedSimpleSeq l -> l |> List.ofArray |> List.collect (missingReferencesInSentence file line missingVariables)
+        | ParsedSentenceNode.ParsedSimpleVariable (s,e,v) -> if missingVariables |> Set.contains v then [makeParseError file line s e (sprintf "Unknown variable %s" v)] else []
+        | _ -> []
+
+    let rec private missingReferencesInFunction file missingAttributes missingVariables missingFunctions node =
+        match node with
+        | ParsedFunctionInvocation (l,s,e,f) -> if missingFunctions |> Set.contains f then [makeParseError file l s e (sprintf "Unknown function %s" f)] else []
         | ParsedChoice (_, l)
         | ParsedSeq (_, l) ->
             l
             |> List.ofArray
-            |> List.collect (fst >> missingFunctionReferences file functionName)
+            |> List.collect
+                (fun (n, c) ->
+                    let refsInFunction = missingReferencesInFunction file missingAttributes missingVariables missingFunctions n
+                    let refsInCondition = missingReferencesInCondition file missingAttributes missingVariables c
+                    List.append refsInFunction refsInCondition)
+        | ParsedSentence(line, n) ->
+            missingReferencesInSentence file line missingVariables n
         | _ -> []
+
+    let private missingReferencesInVariable file missingAttributes missingVariables (variable:ParsedVariableDefinition) =
+        variable.Result
+        |> List.ofArray
+        |> List.collect
+            (fun value ->
+                missingReferencesInCondition file missingAttributes missingVariables value.Condition.Condition )
+
+    let private missingReferencesInAttribute file missingAttributes (attribute:ParsedAttributeDefinition) =
+        attribute.Result
+        |> List.ofArray
+        |> List.collect
+            (fun value ->
+                missingReferencesInCondition file missingAttributes Set.empty value.Condition)
 
     /// Link together a set of compiled modules to make a template ready for passing errors back to the user or generating texts.
     let link (modules:CompiledModule list) : Template =
@@ -117,38 +157,53 @@ module Linker =
             |> List.collect (findCircularReferenceErrors knownFiles [])
 
         // Find any entities that are used but cannot be found in an import.
-        let missingFunctionErrors, missingAttributeErrors, missingVariableErrors =
-            let f =
-                modules
-                |> List.map
-                    (fun m ->
-                        // Recursively find all of the imported modules.
-                        let allImportedModules              = findImportedModules knownFiles m
-                        let allPublicFunctions              = allImportedModules |> List.collect (fun m -> m.PublicFunctions |> List.map (fun f -> f.Name)) |> Set.ofList
-                        let allVariables                    = allImportedModules |> List.collect (fun m -> m.Variables |> List.map (fun f -> f.Name)) |> Set.ofList
-                        let missingPublicFunctions          = m.RequiredFunctions |> List.filter (fun f -> allPublicFunctions |> Set.contains f |> not)
-                        let missingPublicFunctionErrors     =
-                            missingPublicFunctions
+        let missingEntityErrors =
+            modules
+            |> List.collect
+                (fun m ->
+                    // Recursively find all of the imported modules.
+                    let allImportedModules              = findImportedModules knownFiles m
+                    let allPublicFunctions              = allImportedModules |> List.collect (fun m -> m.PublicFunctions |> List.map (fun f -> f.Name)) |> Set.ofList
+                    let allVariables                    = allImportedModules |> List.collect (fun m -> m.Variables |> List.map (fun f -> f.Name)) |> Set.ofList
+                    let allAttributes                   = allImportedModules |> List.collect (fun m -> m.Attributes |> List.map (fun f -> f.Name)) |> Set.ofList
+                    let missingPublicFunctions          = m.RequiredFunctions |> List.filter (fun f -> allPublicFunctions |> Set.contains f |> not) |> Set.ofList
+                    let missingVariables                = m.RequiredVariables |> List.filter (fun f -> allVariables |> Set.contains f |> not) |> Set.ofList
+                    let missingAttributes               = m.RequiredAttributes |> List.filter (fun f -> allAttributes |> Set.contains f |> not) |> Set.ofList
+                    if (missingPublicFunctions |> Set.isEmpty && missingVariables |> Set.isEmpty && missingAttributes |> Set.isEmpty) then []
+                    else
+                        let attributesToInspect = m.Attributes |> List.filter (fun a -> a.Dependencies |> Array.tryFind (function | ParsedAttributeName att -> missingAttributes |> Set.contains att | _ -> false) |> Option.isSome)
+                        let variablesToInspect = m.Variables |> List.filter (fun a -> a.Dependencies |> Array.tryFind (function | ParsedAttributeName att -> missingAttributes |> Set.contains att | ParsedVariableName v -> missingVariables |> Set.contains v) |> Option.isSome)
+                        let functionsToInspect =
+                            List.append m.PrivateFunctions m.PublicFunctions
+                            |> List.filter
+                                (fun a ->
+                                    a.Dependencies
+                                    |> Array.tryFind
+                                        (function   | ParsedAttributeRef att -> missingAttributes |> Set.contains att
+                                                    | ParsedVariableRef v -> missingVariables |> Set.contains v
+                                                    | ParsedFunctionRef f -> missingPublicFunctions |> Set.contains f)
+                                    |> Option.isSome)
+                        let functionMissingRefs =
+                            functionsToInspect
                             |> List.collect
                                 (fun f ->
-                                    m.PublicFunctions
-                                    |> List.filter (fun fn -> fn.Dependencies |> Array.tryFind (function | ParsedFunctionRef f2 -> f = f2 | _ -> false) |> Option.isSome)
-                                    |> List.collect (fun fn -> missingFunctionReferences m.File f fn.Tree))
-                        let missingVariables                = m.RequiredVariables |> List.filter (fun f -> allVariables |> Set.contains f |> not)
-                        let missingVariableErrors           =
-                            missingVariables
+                                    missingReferencesInFunction m.File missingAttributes missingVariables missingPublicFunctions f.Tree)
+                        let attributeMissingRefs =
+                            attributesToInspect
                             |> List.collect
-                                (fun v ->
-                                    m.PublicFunctions
-                                    |> List.filter (fun fn -> fn.Dependencies |> Array.tryFind (function | ParsedFunctionRef f2 -> f = f2 | _ -> false) |> Option.isSome)
-                                    |> List.collect (fun fn -> missingFunctionReferences m.File f fn.Tree))
-                        ())
-            [], [], []
+                                (fun f ->
+                                    missingReferencesInAttribute m.File missingAttributes f)
+                        let variableeMissingRefs =
+                            variablesToInspect
+                            |> List.collect
+                                (fun f ->
+                                    missingReferencesInVariable m.File missingAttributes missingVariables f)
+                        functionMissingRefs @ attributeMissingRefs @ variableeMissingRefs)
 
         // Report any errors in imported files to the parent.
 
         // This should be all the errors we can possibly encounter. Report them and bail if there are any.
-        let allErrors = (modules |> List.collect (fun m -> m.Errors)) @ unknownReferenceErrors @ publicFunctionClashErrors @ attributeClashErrors @ variableClashErrors @ circularReferenceErrors @ missingFunctionErrors @ missingAttributeErrors @ missingVariableErrors
+        let allErrors = (modules |> List.collect (fun m -> m.Errors)) @ unknownReferenceErrors @ publicFunctionClashErrors @ attributeClashErrors @ variableClashErrors @ circularReferenceErrors @ missingEntityErrors
         let allWarnings = modules |> List.collect (fun m -> m.Warnings)
         if allErrors.Length > 0 then
             {
